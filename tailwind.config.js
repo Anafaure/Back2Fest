7cbee8a3ed9b51f7f42b5a80e7d2c08924f18e94
Anafaure <anaelle.faure@epitech.digital>
2024-05-06 15:51:11 +0200

    [ADD] Site Echo and site Melodica

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Black Heat Free", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["Super Easy", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
