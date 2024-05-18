a8e54b5b08171dc50c4e453861bf821b42f8af4d
guillaumebozec <guillaume.bozec@epitech.digital>
2024-05-18 14:26:10 +0200

    [PUT] Home page responsive

--- App/screens/homeScreen.jsx
+++ App/screens/homeScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, useWindowDimensions } from 'react-native';
 import { ref, onValue, off, database } from '../config/firebase';
 import { useAuth } from '../hooks/useAuth';
 import { useNavigation } from '@react-navigation/native';
@@ -8,6 +8,7 @@ export default function HomeScreen() {
   const [artists, setArtists] = useState([]);
   const { user } = useAuth();
   const navigation = useNavigation();
+  const { width, height } = useWindowDimensions();
 
   useEffect(() => {
     const artistsRef = ref(database, '/artistes');
@@ -18,7 +19,7 @@ export default function HomeScreen() {
           const data = snapshot.val();
           const allArtists = Object.keys(data).map((key) => ({
             artistName: data[key].nom,
-            imageUrl: data[key].image || 'https://via.placeholder.com/100', // Fallback placeholder if image is not available
+            imageUrl: data[key].image || 'https://via.placeholder.com/100',
           }));
           setArtists(allArtists);
         }
@@ -37,111 +38,131 @@ export default function HomeScreen() {
     );
   }
 
-  return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <Text style={styles.title}>Raccourcis</Text>
-      <View style={styles.sosmap}>
-        <Text style={styles.sos}>S.O.S</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('Map')}>
-          <Image
-            style={styles.map}
-            source={{ uri: "https://i.pinimg.com/736x/c4/aa/29/c4aa29390e06dbc542bbcd498a19546e.jpg" }}
-          />
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.title}>Aujourd'hui</Text>
-      <Text style={styles.subtitle}>Artistes</Text>
-      <ScrollView horizontal>
-        {artists.map((artist, index) => (
-          <TouchableOpacity key={index} onPress={() => navigation.navigate('Artist', { artistName: artist.artistName })}>
-            <View style={styles.card}>
-              <Image
-                style={styles.cardImage}
-                source={{ uri: artist.imageUrl }}
-              />
-              <Text style={styles.cardText}>{artist.artistName}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </ScrollView>
+  const responsiveStyles = createResponsiveStyles(width, height);
 
-      <Text style={styles.subtitle}>Activités</Text>
-      <ScrollView horizontal>
-        {artists.map((artist, index) => (
-          <View style={styles.card} key={index}>
-            <Image
-              style={styles.cardImage}
-              source={{ uri: artist.imageUrl }}
-            />
-            <Text style={styles.cardText}>{artist.artistName}</Text>
+  return (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={responsiveStyles.scrollContainer}>
+        <Text style={responsiveStyles.title}>Raccourcis</Text>
+        <View style={responsiveStyles.sosmap}>
+          <View style={responsiveStyles.sosContainer}>
+            <Text style={responsiveStyles.sos}>S.O.S</Text>
           </View>
-        ))}
+          <TouchableOpacity onPress={() => navigation.navigate('Map')}>
+            <Image
+              style={responsiveStyles.map}
+              source={{ uri: "https://i.pinimg.com/736x/c4/aa/29/c4aa29390e06dbc542bbcd498a19546e.jpg" }}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text style={responsiveStyles.title}>Aujourd'hui</Text>
+        <Text style={responsiveStyles.subtitle}>Artistes</Text>
+        <ScrollView horizontal>
+          {artists.map((artist, index) => (
+            <TouchableOpacity key={index} onPress={() => navigation.navigate('Artist', { artistName: artist.artistName })}>
+              <View style={responsiveStyles.card}>
+                <Image
+                  style={responsiveStyles.cardImage}
+                  source={{ uri: artist.imageUrl }}
+                />
+                <Text style={responsiveStyles.cardText} numberOfLines={1} ellipsizeMode="tail">{artist.artistName}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+
+        <Text style={responsiveStyles.subtitle}>Activités</Text>
+        <ScrollView horizontal>
+          {artists.map((artist, index) => (
+            <TouchableOpacity key={index} >
+              <View style={responsiveStyles.card}>
+                <Image
+                  style={responsiveStyles.cardImage}
+                  source={{ uri: artist.imageUrl }}
+                />
+                <Text style={responsiveStyles.cardText} numberOfLines={1} ellipsizeMode="tail">{artist.artistName}</Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </ScrollView>
-    </ScrollView>
+    </View>
   );
 }
 
-// Styles for the component
-const styles = StyleSheet.create({
+const createResponsiveStyles = (width, height) => StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    paddingHorizontal: 20,
-    paddingBottom: 30,
+    paddingHorizontal: width * 0.05,
+    paddingBottom: height * 0.03,
     backgroundColor: '#121212',
   },
   title: {
     color: '#FDFDFD',
-    fontSize: 36,
+    fontSize: width * 0.09, // Ajusté pour la largeur de l'écran
     fontWeight: '600',
-    marginBottom: 10,
-    marginTop: 50,
+    marginBottom: height * 0.01,
+    marginTop: height * 0.05,
   },
   subtitle: {
     color: '#FDFDFD',
-    fontSize: 24,
+    fontSize: width * 0.06, // Ajusté pour la largeur de l'écran
     fontWeight: '600',
-    marginBottom: 5,
+    marginBottom: height * 0.01,
   },
   card: {
     backgroundColor: '#FDFDFD',
     borderRadius: 10,
-    padding: 10,
+    padding: width * 0.025,
     alignItems: 'center',
-    width: 150,
+    width: width * 0.4, // Ajusté pour la largeur de l'écran
     justifyContent: 'center',
-    marginHorizontal: 5,
-    marginBottom: 20,
+    marginHorizontal: width * 0.01,
+    marginBottom: height * 0.02,
+    height: height * 0.20, // Fixe la hauteur des cartes
   },
   cardText: {
     color: 'black',
-    fontSize: 16,
+    fontSize: width * 0.04, // Ajusté pour la largeur de l'écran
     fontWeight: '600',
     textTransform: 'uppercase',
     textAlign: 'center',
   },
   cardImage: {
-    width: 100,
-    height: 100,
-    marginBottom: 10,
+    width: width * 0.25, // Ajusté pour la largeur de l'écran
+    height: width * 0.25, // Ajusté pour la largeur de l'écran
+    marginBottom: height * 0.01,
+  },
+  sosContainer: {
+    width: width * 0.40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E12020',
+    padding: width * 0.06, // Réduit la taille du padding
+    borderRadius: 10,
+    marginRight: width * 0.08, // Ajouter plus d'espace entre SOS et la carte
   },
   sos: {
     color: '#FDFDFD',
-    fontSize: 48,
+    fontSize: width * 0.1, // Réduit la taille de la police
     fontWeight: '700',
     textAlign: 'center',
-    backgroundColor: '#E12020',
-    padding: 30,
-    borderRadius: 10,
-    overflow: 'hidden',
   },
   sosmap: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 20,
+    justifyContent: 'space-between',
+    marginBottom: height * 0.03, // Ajouter plus d'espace en dessous
   },
   map: {
-    width: 180,
-    height: 150,
+    width: width * 0.40, // Réduit la largeur de la carte
+    height: height * 0.18, // Réduit la hauteur de la carte
     borderRadius: 10,
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
   },
 });
